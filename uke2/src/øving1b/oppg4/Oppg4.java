90bda645dbd9f363f3d48f91eea6c9abf7b14e4a
Spirevipp <bjornar.helgeland@hotmail.com>
2023-01-10 19:22:22 +0100

    øving1b oppg4

--- uke2/src/øving1b/oppg4/Oppg4.java
+++ uke2/src/øving1b/oppg4/Oppg4.java
@@ -1,7 +1,10 @@
 package øving1b.oppg4;
 
+import java.util.Arrays;
+
 public class Oppg4 {
 	// Hvor i programmet er x-en nedenfor gyldig?
+	// ^^ ga ingen mening?
 	static int f(int x) {
 		x = 2 * x;
 		return x;
@@ -20,13 +23,19 @@ public class Oppg4 {
 		int a = 4;
 		int b = f(a);
 		// Hva er a og b nå?
+		// a = 4, b = 8
+		System.out.println("a: " + a + " - b: " + b);
+
 		double[] tab = {2.0, 5.0};
 		g(tab);
 		// Hva er innholdet i tab nå?
+		// tab = [4.0, 10.0]
+		System.out.println("tab: " + Arrays.toString(tab));
+
 		// Hvilke av disse setningene er lovlige?
-		double c1 = f(3.0);
+		// double c1 = f(3.0); // ikke lov
 		double c2 = f(3);
-		int c3 = f(3.0);
+		// int c3 = f(3.0); // ikke lov
 		int c4 = f(3);
 	}
 }
