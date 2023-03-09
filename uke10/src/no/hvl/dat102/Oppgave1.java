05d44a904af31526aeca72bb5230d1b26c8d71f3
Spirevipp <bjornar.helgeland@hotmail.com>
2023-03-09 16:00:14 +0100

    Flyttet duplisert kode ut fra main til egne metoder

--- uke10/src/no/hvl/dat102/Oppgave1.java
+++ uke10/src/no/hvl/dat102/Oppgave1.java
@@ -5,16 +5,28 @@ import java.time.Instant;
 import java.util.Random;
 
 public class Oppgave1 {
+	public static void main(String[] args) {
+		int   gjIterasjoner = 5;
+		int   antallnumre   = 50000;
+		int[] randomseeds   = {12345, 67890, 13579, 24680, 69420};
 
-	/**
-	 * Sortering ved innsetting (Insertion sort)
-	 */
-	public static <T extends Comparable<? super T>> void sorteringVedInssetting(T[] a, int n) {
-		sorteringVedInssetting(a, 0, n - 1);
+		long gjennomsnitt1 = sorterOgTaTid(gjIterasjoner, antallnumre, randomseeds, sortmetode.STANDARD);
+		System.out.println("Gjennomsnitt for standard metode: " + gjennomsnitt1 + "ms");
+
+		long gjennomsnitt2 = sorterOgTaTid(gjIterasjoner, antallnumre, randomseeds, sortmetode.SNARVEI);
+		System.out.println("Gjennomsnitt for snarvei metode: " + gjennomsnitt2 + "ms");
+		// vi har observert at metoden med å finne minst og sette den på posisjon 0 er raskere enn den vanlige metoden
 	}
 
-	public static <T extends Comparable<? super T>> void sorteringMedSnarvei(T[] a, int n) {
-
+	/**
+	 * Hjeplemetode for snarvei sortering fra oppgave a.<br>
+	 * Denne vil starte ved n-te element og jobbe seg framover i tabellen,
+	 * og bytte posisjon på minste element og første element
+	 * @param a Tabellen som skal jobbes med
+	 * @param n Hvor mange elementer som skal tas med ved sortering i tabellen
+	 * @param <T> Hvilken som helst type element som utvider Comparable klassen
+	 */
+	private static <T extends Comparable<? super T>> void flyttMinsteFoerst(T[] a, int n) {
 		int pos   = n - 1;
 		T   minst = a[n - 1];
 		for (int i = n - 1; i > 0; i--) {
@@ -26,12 +38,46 @@ public class Oppgave1 {
 		}
 		a[pos] = a[0];
 		a[0]   = minst;
+	}
 
+	/**
+	 * Flytter minste element i tabellen til indeks 0, deretter gjør en sortering
+	 * @param a Tabellen som skal jobbes med
+	 * @param n Hvor mange elementer som skal tas med ved sortering i tabellen
+	 * @param <T> Hvilken som helst type element som utvider Comparable klassen
+	 */
+	public static <T extends Comparable<? super T>> void sorteringMedSnarvei(T[] a, int n) {
+		flyttMinsteFoerst(a, n);
+		sorteringVedInssetting(a, 0, n - 1);
+	}
+
+	/**
+	 * Omskrevet versjon av sortering med innsetting som flytter 2 elementer om gangen.<br>
+	 * Oppgave b
+	 * @param a Tabellen som skal jobbes med
+	 * @param n Hvor mange elementer som skal tas med ved sortering i tabellen
+	 */
+	private static void sorteringMedDobbel(Integer[] a, int n) {
+	}
+
+	/**
+	 * Sortering ved innsetting (Insertion sort)
+	 *
+	 * @param a Tabellen som skal jobbes med
+	 * @param n Hvor mange elementer som skal tas med ved sortering i tabellen
+	 * @param <T> Hvilken som helst type element som utvider Comparable klassen
+	 */
+	public static <T extends Comparable<? super T>> void sorteringVedInssetting(T[] a, int n) {
 		sorteringVedInssetting(a, 0, n - 1);
 	}
 
 	/**
 	 * Sorterer ein del av tabellen, start ... slutt (begge grensene er med)
+	 *
+	 * @param a Tabellen som skal jobbes med
+	 * @param start Hvor sorteringen starter
+	 * @param slutt Hvor sorteringen slutter
+	 * @param <T> Hvilken som helst type element som utvider Comparable klassen
 	 */
 	public static <T extends Comparable<? super T>> void sorteringVedInssetting(T[] a, int start, int slutt) {
 		for (int i = start + 1; i <= slutt; i++) {
@@ -47,55 +93,64 @@ public class Oppgave1 {
 					ferdig = true;
 				}
 			}
-
 			a[j + 1] = tmp;
 		}
 	}
 
-	public static void main(String[] args) {
-		int  gjIterasjoner = 5;
-		long totalTid1     = 0;
-		int  antallnumre   = 50000;
-		int[] randomseeds = {12345, 67890, 13579, 24680, 69420};
-		for (int i = 0; i < gjIterasjoner; i++) {
-			Integer[] tabell    = new Integer[antallnumre];
-			Random    tilfeldig = new Random(randomseeds[i]);
+	/**
+	 * Hjelpemetode for å opprette en tabell med tilfeldige tall i tilfeldig rekkefølge
+	 * @param antall Hvor mange tilfeldige tall som skal genereres
+	 * @param randomseed Hvilken randomseed som skal brukes ved generering av tilfeldige tall
+	 *
+	 * @return En tabell med (antall) tilfeldige tall i tilfeldig rekkefølge
+	 */
+	private static Integer[] settOppTabell(int antall, int randomseed) {
+		Integer[] tabell    = new Integer[antall];
+		Random    tilfeldig = new Random(randomseed);
+		for (int j = 0; j < antall; j++) {
+			tabell[j] = tilfeldig.nextInt();
+		}
+		return tabell;
+	}
 
-			for (int j = 0; j < antallnumre; j++) {
-				tabell[j] = tilfeldig.nextInt();
-			}
+	/**
+	 * Enkel enum for å enkelt velge hvilken sorteringsmetode som skal brukes<br>
+	 * Strengt tatt ikke nødvendig, men gjør koden litt enklere
+	 */
+	private enum sortmetode {
+		STANDARD, SNARVEI, DOBBEL
+	}
+
+	/**
+	 * Kjører valgt sorteringsmetode (iterasjoner) antall ganger over tabeller tilfedig generert av spesifiserte randomseeds
+	 * @param iterasjoner Hvor mange ganger sortering skal gjøres for å regne gjennomsnitt
+	 * @param antallnumre Hvor mange numre tabell som skal sorteres skal inneholde
+	 * @param randomseeds Hvilke randomseeds som skal brukes. Denne tabellen må inneholde minst like mange seeds som det er iterasjoner.
+	 * @param sm Hvilken sorteringsmetode som skal brukes
+	 *
+	 * @return Tiden det tok å kjøre (iterasjoner) antall ganger av valgt sorteringsmetode på tabell av størrelse (antallnumre)
+	 */
+	private static long sorterOgTaTid(int iterasjoner, int antallnumre, int[] randomseeds, sortmetode sm) {
+		long totalTid = 0;
+		System.out.println("Sortering med metode: " + sm.toString());
+		System.out.print("Tider: ");
+		for (int i = 0; i < iterasjoner; i++) {
+			Integer[] tabell = settOppTabell(antallnumre, randomseeds[i]);
+
 			Instant start = Instant.now();
-
-			sorteringVedInssetting(tabell, tabell.length);
+			switch (sm) {
+				case STANDARD -> sorteringVedInssetting(tabell, tabell.length);
+				case SNARVEI -> sorteringMedSnarvei(tabell, tabell.length);
+				case DOBBEL -> sorteringMedDobbel(tabell, tabell.length);
+			}
 			Instant finish = Instant.now();
 			long tid = Duration.between(start, finish)
 			                   .toMillis();
-			totalTid1 += tid;
-			System.out.println("Iterasjon " + i + ": " + tid);
+
+			totalTid += tid;
+			System.out.print(tid + "ms, ");
 		}
-		long gjennomsnitt1 = totalTid1 / gjIterasjoner;
-		System.out.println("Gjennomsnitt 1: " + gjennomsnitt1);
-
-		long totalTid2 = 0;
-		for (int i = 0; i < gjIterasjoner; i++) {
-			Integer[] tabell    = new Integer[antallnumre];
-			Random    tilfeldig = new Random(randomseeds[i]);
-
-			for (int j = 0; j < antallnumre; j++) {
-				tabell[j] = tilfeldig.nextInt();
-			}
-			Instant start = Instant.now();
-
-			sorteringMedSnarvei(tabell, tabell.length);
-			Instant finish = Instant.now();
-			long tid = Duration.between(start, finish)
-			                   .toMillis();
-			totalTid2 += tid;
-			System.out.println("Iterasjon " + i + ": " + tid);
-		}
-		long gjennomsnitt2 = totalTid2 / gjIterasjoner;
-		System.out.println("Gjennomsnitt 2: " + gjennomsnitt2);
-		System.out.println("Gjennomsnitt 1: " + gjennomsnitt1);
-		// vi har observert at metoden med å finne minst og sette den på posisjon 0 er raskere enn den vanlige metoden
+		System.out.println();
+		return totalTid / iterasjoner;
 	}
 }
