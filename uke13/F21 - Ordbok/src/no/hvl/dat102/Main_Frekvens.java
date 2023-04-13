126a5c2c1303a0bcf08c03cc5231b741a3f8b7ea
Spirevipp <bjornar.helgeland@hotmail.com>
2023-04-13 14:17:35 +0200

    Lagt til F21

---
+++ uke13/F21 - Ordbok/src/no/hvl/dat102/Main_Frekvens.java
@@ -0,0 +1,32 @@
+﻿package no.hvl.dat102;
+
+import java.util.Iterator;
+
+public class Main_Frekvens {
+	public static void main(String[] args) {
+		String[] ord = {"er", "det", "finn", "er", "finn", "er"};
+		
+		// Lag ordbok
+		OrdbokInterface<String, Integer> frekvenser = new Ordbok<>();
+		
+		// Tell opp
+		for (String s : ord) {
+			if (frekvenser.inneholder(s)) {
+				int f = frekvenser.getVerdi(s);
+				frekvenser.leggTil(s, f + 1);
+			} else {
+				frekvenser.leggTil(s, 1);
+			}
+		}
+		
+		// Skriv ut frekvensoversikt
+		Iterator<String> ordit = frekvenser.getNokkelIterator();
+		Iterator<Integer> frekvensit = frekvenser.getVerdiIterator();
+		
+		while (ordit.hasNext()) {
+			System.out.println(ordit.next() + ": " + frekvensit.next());
+		}
+		
+	}
+
+}
